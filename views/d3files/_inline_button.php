32f6264582ea34a28dadfb95d097868097efb6d4
uldisn <uldisnelsons@gmail.com>
2019-11-28 17:34:16 +0200

    fixed bug "Undefined variable: previewAttrs"
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

--- views/d3files/_inline_button.php
+++ views/d3files/_inline_button.php
@@ -6,5 +6,7 @@ use yii\helpers\Html;
  * @var string $icon
  * @var array $previewAttrs
  */
-
+if(!isset($previewAttrs)){
+    $previewAttrs = [];
+}
 echo Html::a('<span class="' . $icon . '"></span>', 'javascript:void(0)', $previewAttrs);

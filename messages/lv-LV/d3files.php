d1709ecc6672a3b6ea4efe33b670714cfa27b935
lauris@weberp.lv <latpages@gmail.com>
2020-02-29 13:26:54 +0200

    i18n - missing translation fix

--- messages/lv-LV/d3files.php
+++ messages/lv-LV/d3files.php
@@ -17,6 +17,7 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
+    'Attachment error' => 'Pielikuma kļūda',
     'Cannot upload the file' => 'Failu neizdevās augšuplādēt',
     'File uploaded successfully.' => 'Fails veiksmīgi augšuplādēts',
     'File not uploaded.' => 'Fails netika augšuplādēts',
